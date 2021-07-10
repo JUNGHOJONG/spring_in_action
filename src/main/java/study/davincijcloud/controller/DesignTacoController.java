f0b01101555309c17550dae9c029ef06170218bf
JUNGHOJONG <hojong1351@naver.com>
2021-07-11 00:35:36 +0900

    2장 웹 애플리케이션 개발 진행중(2.1.3 뷰 디자인하기 미완료)

---
+++ src/main/java/study/davincijcloud/controller/DesignTacoController.java
@@ -0,0 +1,43 @@
+package study.davincijcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import study.davincijcloud.domain.Ingredient;
+import study.davincijcloud.domain.Taco;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequestMapping("/design")
+@Controller
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP)
+        );
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+
+        model.addAttribute("taco", new Taco());
+
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+}
