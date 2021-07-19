55f6d8ba0f53b0a69e1f5c688073a46f50008b3e
JUNGHOJONG <hojong1351@naver.com>
2021-07-19 19:16:01 +0900

    JDBC 기반의 사용자 스토어(2)
    - 암호화된 비밀번호 사용하기

--- src/main/java/study/davincijcloud/security/SecurityConfig.java
+++ src/main/java/study/davincijcloud/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -47,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authoritiesByUsernameQuery(
                         "select username, authority from authorities " +
                                 "where username=?"
-                );
+                )
+                .passwordEncoder(new NoEncodingPasswordEncoder());
     }
 }
