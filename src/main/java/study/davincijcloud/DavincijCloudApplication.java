0c668f7c840d61f8ca6539de88104165be5afe75
JUNGHOJONG <hojong1351@naver.com>
2021-07-25 01:15:05 +0900

    5.3.3 프로파일을 사용해서 조건별로 빈 생성하기까지 완료(5장 완료)

--- src/main/java/study/davincijcloud/DavincijCloudApplication.java
+++ src/main/java/study/davincijcloud/DavincijCloudApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import study.davincijcloud.data.IngredientRepository;
 import study.davincijcloud.domain.Ingredient;
 import study.davincijcloud.domain.Ingredient.Type;
@@ -17,6 +18,12 @@ public class DavincijCloudApplication {
     }
 
     @Bean
+    /*
+    @Profile("dev") // 프로덕션 환경에서 식자재 데이터 로드되는 것 방지
+    @Profile({"dev", "qa"}) // dev 프로파일이나 qa 프로파일 중 하나가 활성화될 때 식자재 데이터 로드
+    @Profile("!prod") // prod 프로파일이 활성화되지 않을 경우 식자재 데이터 로드
+    */
+    @Profile({"!prop", "!qa"}) // prod 프로파일과 qa 프로파일 모두 활성화되지 않을 경우 식자재 데이터 로드
     public CommandLineRunner dataLoader(IngredientRepository repo) {
         return new CommandLineRunner() {
             @Override
