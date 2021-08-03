c9fe469913105910e1518d6a54513c38486bf456
JUNGHOJONG <hojong1351@naver.com>
2021-08-03 17:30:50 +0900

    Ch06 디렉토리 재생성

--- src/main/java/study/davincijcloud/data/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package study.davincijcloud.data;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import study.davincijcloud.domain.Taco;
-
-public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
-//    Taco save(Taco design);
-}
--- src/main/java/study/davincijcloud/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package study.davincijcloud.data;
-
-import org.springframework.data.repository.CrudRepository;
-import study.davincijcloud.domain.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
